0b7b77693b022ca37dbd5664a0466d354e1372b6
jakesheinberg <23jsheinberg@athenian.org>
2020-10-08 07:32:19 +0000

    news story complete begin work on investment mechanism

--- my_code.py
+++ my_code.py
@@ -4,7 +4,7 @@ import random
 
 print("Welcome to a stock market simulator!")
 print("In this realistic simulation of the stock market you will be given news stories, relevant trends, and chances to commit insider trading or fraud, don't get caught!")
-print("You will adjust or add to you investments every simulated week. You begin with a $1000, you have 15 weeks to accumulate as much money as possible.")
+print("You will adjust or add to you investments every simulated week. You begin with a $10,000, you have 5 weeks to accumulate as much money as possible.")
 print("Good luck!!")
 z=0
 while z<5:
@@ -12,8 +12,8 @@ while z<5:
     z=z+1
 
 def news_story():
-    news_number=random.randint(1,8)
-    news_number2=random.randint(1,8)
+    news_number=random.randint(1,11)
+    news_number2=random.randint(1,11)
     if news_number==1 or news_number2==1:
         print(f"There is a hurricane coming! Get ready to stock up and stay at home!")
         hurricane=True
@@ -35,18 +35,25 @@ def news_story():
     elif news_number==7 or news_number2==7:
         print("Strict restrictions placed on gas cars all around the US")
         gas_car_restrictions=True
+    elif news_number==8 or news_number2==8:
+        print("Competing between social networks lead to a boom in social media use")
+        social_use=True
+    elif news_number==9 or news_number2==9:
+        print("Farmers all around the United States going bankrupt")
+        farmer_bankrupcy=True
+    elif news_number==10 or news_number2==10:
+        print("Schools have been shutdown due to a strike sparked by teachers!")
+        school_cancelled:True
 
 
 
         
-news_story()
-t=0
-while t<2:
-    print("..")
-    t=t+1
-news_story()
 
-
+def investments():
+    print("Welcome to the market! You have 10,000 dollars to spend. Save it, spend it, risk it!")
+    control_center=input("Type '1' to see weekly news storys, type '2' to see availible stocks and prices, and type 3 to insider trade!")
+    if x==1:
+        news_story
 
 
 
